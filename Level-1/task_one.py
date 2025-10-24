8af8698489d4c1b88028b076ccf7a3c6001320df
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-24 14:33:43 +0100

    create generate_receipt function

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -12,7 +12,7 @@ def format_money(price: int) -> str:
     if price == 0:  # EDGE CASE 'item should be free'
         return 'Free'
 
-    return f"{price:.2f}"
+    return f"£{price:.2f}"
 
 
 def add_to_basket(item: dict) -> list:
@@ -21,15 +21,23 @@ def add_to_basket(item: dict) -> list:
 
 
 def generate_receipt(basket: list) -> str:
+    if len(basket) == 0:  # EDGE CASE empty basket
+        return "Basket is empty"
+    receipt_to_send = ''
+    total = 0
     for item in basket:
-        formatted_money = format_money(item['price'])
-        return  # return the receipt string
+        total += item['price']
+        receipt_to_send += item['name'] + ' - ' + \
+            format_money(item['price']) + '\n'
+
+    receipt_to_send += f"Total: {format_money(total)}"
+    return receipt_to_send  # return the receipt string
 
 
 if __name__ == "__main__":
     add_to_basket({
         "name": "Bread",
-        "price": 0
+        "price": 1.80
     })
     add_to_basket({
         "name": "Milk",
