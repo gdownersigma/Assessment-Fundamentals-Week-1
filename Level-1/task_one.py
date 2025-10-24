518978ff7683daeb8f9a91cef7b4346ea656e6f0
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-24 14:19:36 +0100

    add function to format money & handle 'Free' Edge case

--- Level-1/task_one.py
+++ Level-1/task_one.py
@@ -7,26 +7,36 @@ The only variable you are allowed to use in the global scope is the basket below
 basket = []
 
 
+def format_money(price: int) -> str:
+    """Returns a string of the nicely formatted price."""
+    if price == 0:  # EDGE CASE 'item should be free'
+        return 'Free'
+
+    return f"{price:.2f}"
+
+
 def add_to_basket(item: dict) -> list:
     basket.append(item)
     return basket
 
 
 def generate_receipt(basket: list) -> str:
-    return  # return the receipt string
+    for item in basket:
+        formatted_money = format_money(item['price']))
+            return  # return the receipt string
 
 
-if __name__ == "__main__":
-    add_to_basket({
-        "name": "Bread",
-        "price": 1.80
-    })
-    add_to_basket({
-        "name": "Milk",
-        "price": 0.80
-    })
-    add_to_basket({
-        "name": "Butter",
-        "price": 1.20
-    })
-    print(generate_receipt(basket))
+            if __name__ == "__main__":
+            add_to_basket({
+                "name": "Bread",
+                "price": 0
+            })
+            add_to_basket({
+                "name": "Milk",
+                "price": 0.80
+            })
+            add_to_basket({
+                "name": "Butter",
+                "price": 1.20
+            })
+            print(generate_receipt(basket))
