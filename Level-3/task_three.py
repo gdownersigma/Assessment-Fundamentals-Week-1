11c268c9d57958f3e3a74e9500d17d5c30732af8
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-24 16:42:35 +0100

    better comments / doctstrings

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -4,23 +4,24 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 
 
 def get_price_from_receipt(receipt_line: str) -> int:
-    """returns the price from the receipt"""
+    """Returns the price from the receipt."""
     return float(receipt_line.split("£")[1])
 
 
 def calculate_price_after_vat(price: int) -> int:
-    """Return the new VAT"""
+    """Return the new VAT."""
     return price*0.8
 
 
 def create_tax_receipt(tax_info: list[list], old_total: int) -> str:
-    """Returns the tax receipt from delivered info"""
+    """Returns the tax receipt from delivered info."""
 
     formatted_receipt = 'VAT RECEIPT\n'
     new_total = 0
     if old_total > 0:
         formatted_receipt += '\n'
         for line in tax_info:
+            # replacing old price with new price
             new_line = line[0].split("£")[0]+"£" + f"{line[1]:.2f}"
             new_total += line[1]
             formatted_receipt += new_line + '\n'
@@ -29,12 +30,12 @@ def create_tax_receipt(tax_info: list[list], old_total: int) -> str:
 
 
 def generate_invoice(receipt: str) -> str:
-    """function to return the generated invoice string"""
+    """Function to return the generated invoice string."""
     receipt_lines = receipt.split('\n')
-    if len(receipt_lines) == 1:
+    if len(receipt_lines) == 1:  # EDGE CASE receipt is empty
         return create_tax_receipt([0, 0], 0)
-    tax_info = []
 
+    tax_info = []
     old_total = 0
 
     for line in receipt_lines[:-1]:
@@ -47,10 +48,6 @@ def generate_invoice(receipt: str) -> str:
 
 
 if __name__ == "__main__":
-    #     receipt_string = """Bread x 2 - £3.60
-    # Milk x 1 - £0.80
-    # Butter x 1 - £1.20
-    # Total: £5.60"""
     receipt_string = """Bread x 1000 - £3600.00
 Total: £3600.00"""
     print(generate_invoice(receipt_string))
