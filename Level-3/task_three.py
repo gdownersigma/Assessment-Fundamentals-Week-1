cab6a0e0220f72a8b444f2ee8d0265b7aa2ac8af
George <trainee.george.downer@sigmalabs.co.uk>
2025-10-24 15:58:31 +0100

    make useful functions for generating tax

--- Level-3/task_three.py
+++ Level-3/task_three.py
@@ -3,7 +3,29 @@ Note: Do not add ANY variables to the global scope. This WILL break the tests.
 """
 
 
+def get_price_from_receipt(receipt_line: str) -> int:
+    """returns the price from the receipt"""
+    return float(receipt_line[-4:])
+
+
+def calculate_price_after_vat(price: int) -> int:
+    """Return the new VAT"""
+    return price*0.8
+
+
 def generate_invoice(receipt_string: str) -> str:
+    receipt_lines = receipt_string.split('\n')
+    tax_info = []
+
+    old_total = 0
+
+    for line in receipt_lines[:-1]:
+        price = get_price_from_receipt(line)
+        old_total += price
+        new_price = calculate_price_after_vat(price)
+        tax_info.append([line, new_price])
+
+    print(tax_info)
 
     return  # return the invoice string
 
